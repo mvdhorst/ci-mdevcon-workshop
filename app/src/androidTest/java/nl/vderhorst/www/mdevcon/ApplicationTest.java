b8bdb5dddf0b75ac0cb445c9015ae8ba15169d2a
mvdhorstclb <mvdhorst@clb.nl>
2016-03-17 12:29:31 +0100

    Added test coverage

--- app/src/androidTest/java/nl/vderhorst/www/mdevcon/ApplicationTest.java
+++ app/src/androidTest/java/nl/vderhorst/www/mdevcon/ApplicationTest.java
@@ -1,6 +1,9 @@
 package nl.vderhorst.www.mdevcon;
 
 import android.app.Application;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.test.ApplicationTestCase;
 
 /**
@@ -9,5 +12,9 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    public void testHelloWorld() {
+        Espresso.onView(ViewMatchers.withId(R.id.test)).check(ViewAssertions.matches(ViewMatchers.withText("Hello World!")));
     }
 }
