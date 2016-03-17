6bef09fabef3775b193120026188b81cf92b710b
mvdhorstclb <mvdhorst@clb.nl>
2016-03-17 12:36:15 +0100

    100% test coverage

--- app/src/androidTest/java/nl/vderhorst/www/mdevcon/ApplicationTest.java
+++ app/src/androidTest/java/nl/vderhorst/www/mdevcon/ApplicationTest.java
@@ -4,17 +4,20 @@ import android.app.Application;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.matcher.ViewMatchers;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MainActivity> {
     public ApplicationTest() {
-        super(Application.class);
+        super(MainActivity.class);
     }
 
     public void testHelloWorld() {
-        Espresso.onView(ViewMatchers.withId(R.id.test)).check(ViewAssertions.matches(ViewMatchers.withText("Hello World!")));
+        getActivity();
+        Espresso.onView(ViewMatchers.withId(R.id.test)).
+                check(ViewAssertions.matches(ViewMatchers.withText("Hello World!")));
     }
 }
